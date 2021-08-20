7c3acc6bf65307aee1b11d365216a715b8b56b33
Brennon Loveless <brennon.loveless@gmail.com>
2021-08-19 21:55:35 -0700

    Made a lot of progress on the UI including a modal for adding new records

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module timetracker
 
 go 1.16
 
-require github.com/jroimartin/gocui v0.5.0
+require (
+	github.com/jroimartin/gocui v0.5.0
+	github.com/rivo/tview v0.0.0-20210624165335-29d673af0ce2 // indirect
+)
--- main.go
+++ main.go
@@ -1,256 +1,86 @@
-// Copyright 2014 The gocui Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
+// Demo code for the Modal primitive.
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/jroimartin/gocui"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
-type record struct {
-	name string
-}
-
-var editingRecord int
-var records []record
-
-func cursorDown(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		cx, cy := v.Cursor()
-		if cy+1 < len(records) {
-			if err := v.SetCursor(cx, cy+1); err != nil {
-				ox, oy := v.Origin()
-				if err := v.SetOrigin(ox, oy+1); err != nil {
-					return err
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func cursorUp(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		ox, oy := v.Origin()
-		cx, cy := v.Cursor()
-		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
-			if err := v.SetOrigin(ox, oy-1); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func newRecord(g *gocui.Gui, v *gocui.View) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("newRecord", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Editable = true
-
-		if _, err := g.SetCurrentView("newRecord"); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func createRecord(g *gocui.Gui, v *gocui.View) error {
-	var l string
-	var err error
-
-	_, cy := v.Cursor()
-	if l, err = v.Line(cy); err != nil {
-		l = ""
-	}
-
-	records = append(records, record{name: l})
-
-	g.Update(func(g *gocui.Gui) error {
-		v, err := g.View("main")
-		if err != nil {
-			return err
-		}
-
-		v.Clear()
-
-		for _, r := range records {
-			fmt.Fprintln(v, r.name)
-		}
-
-		return nil
-	})
-
-	if err := g.DeleteView("newRecord"); err != nil {
-		return err
-	}
-
-	if _, err := g.SetCurrentView("main"); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func editRecord(g *gocui.Gui, v *gocui.View) error {
-	var l string
-	var err error
-
-	_, cy := v.Cursor()
-	if l, err = v.Line(cy); err != nil {
-		l = ""
-	}
-
-	editingRecord = cy
-
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("editRecord", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Editable = true
-		v.MoveCursor(len(l), 0, true)
-
-		fmt.Fprint(v, l)
-		if _, err := g.SetCurrentView("editRecord"); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func updateRecord(g *gocui.Gui, v *gocui.View) error {
-	var l string
-	var err error
-
-	_, cy := v.Cursor()
-	if l, err = v.Line(cy); err != nil {
-		l = ""
-	}
-
-	records[editingRecord].name = l
-
-	g.Update(func(g *gocui.Gui) error {
-		v, err := g.View("main")
-		if err != nil {
-			return err
-		}
-
-		v.Clear()
-
-		for _, r := range records {
-			fmt.Fprintln(v, r.name)
-		}
-
-		return nil
-	})
-
-	if err := g.DeleteView("editRecord"); err != nil {
-		return err
-	}
-
-	if _, err := g.SetCurrentView("main"); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
-}
-
-func keybindings(g *gocui.Gui) error {
-	if err := g.SetKeybinding("main", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", 'j', gocui.ModNone, cursorDown); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", 'k', gocui.ModNone, cursorUp); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", 'a', gocui.ModNone, newRecord); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("newRecord", gocui.KeyEnter, gocui.ModNone, createRecord); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", gocui.KeyEnter, gocui.ModNone, editRecord); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("editRecord", gocui.KeyEnter, gocui.ModNone, updateRecord); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("main", 0, 0, maxX-1, maxY-3); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Time Tracker"
-		v.Highlight = true
-		v.SelBgColor = gocui.ColorGreen
-		v.SelFgColor = gocui.ColorBlack
-
-		for _, r := range records {
-			fmt.Fprintln(v, r.name)
-		}
-
-		if _, err := g.SetCurrentView("main"); err != nil {
-			return err
-		}
-	}
-
-	if v, err := g.SetView("help", 0, maxY-3, maxX-1, maxY-1); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Frame = false
-		fmt.Fprintln(v, "a) add s) start e) end")
-	}
-
-	return nil
-}
-
 func main() {
-	records = []record{{
-		name: "Record 1",
-	}, {
-		name: "Record 2",
-	}}
+	records := []string{"Record 1", "Record 2", "Record 3"}
 
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
+	timeView := tview.NewTextView().SetText("Some Text")
+	timeView.SetBorder(true).SetTitle("Time")
 
-	g.Cursor = true
+	recordList := tview.NewList().ShowSecondaryText(false)
+	recordList.SetBorder(true).SetTitle("Records").SetBorderPadding(0, 0, 1, 1)
+	recordList.SetChangedFunc(func(index int, mainText string, secondaryText string, shortut rune) {
+		timeView.Clear()
+		timeView.SetText("Time for " + records[index])
+	})
 
-	g.SetManagerFunc(layout)
-
-	if err := keybindings(g); err != nil {
-		log.Panicln(err)
+	for _, record := range records {
+		recordList.AddItem(record, "", 0, nil)
 	}
 
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
+	main := tview.NewFlex().
+		AddItem(recordList, 0, 3, true).
+		AddItem(timeView, 0, 1, false)
+
+	footer := tview.NewTextView().
+		SetTextAlign(tview.AlignLeft).
+		SetText(" a) add record s) start timer e) end timer")
+
+	footer.SetBorderPadding(0, 0, 1, 1)
+
+	app := tview.NewApplication()
+
+	newRecordName := ""
+
+	addRecordForm := tview.NewForm().
+		AddInputField("New Record Name", "", 20, nil, func(text string) {
+			newRecordName = text
+		}).
+		SetButtonsAlign(tview.AlignRight)
+
+	addRecordForm.SetBorder(true).SetBorderColor(tcell.ColorBlue)
+
+	modal := tview.NewFlex().
+		AddItem(nil, 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(nil, 0, 1, false).
+			AddItem(addRecordForm, 7, 1, false).
+			AddItem(nil, 0, 1, false), 40, 1, false).
+		AddItem(nil, 0, 1, false)
+
+	grid := tview.NewGrid().
+		SetRows(0, 1).
+		AddItem(main, 0, 0, 1, 1, 0, 0, true).
+		AddItem(footer, 1, 0, 1, 1, 0, 0, false)
+
+	pages := tview.NewPages().
+		AddPage("main", grid, true, true)
+
+	addRecordForm.
+		AddButton("Save", func() {
+			footer.SetText(newRecordName)
+			pages.RemovePage("modal").ShowPage("main")
+		}).
+		AddButton("Quit", func() {
+			pages.RemovePage("modal").ShowPage("main")
+		})
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 'a' && !pages.HasPage("modal") {
+			pages.AddAndSwitchToPage("modal", modal, true).ShowPage("main")
+			app.SetFocus(addRecordForm)
+			return nil
+		}
+
+		return event
+	})
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
 	}
 }
